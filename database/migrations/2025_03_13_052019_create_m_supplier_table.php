9d29ed06b8071f2958eb4d65940e39755075194b
Malik Adzano Aryasatya Dharmaputera <adzanomalik@gmail.com>
2025-03-13 12:38:10 +0700

    Jobheet 3 praktikum 2.1

--- database/migrations/2025_03_13_052019_create_m_supplier_table.php
+++ database/migrations/2025_03_13_052019_create_m_supplier_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('m_supplier', function (Blueprint $table) {
             $table->id('supplier_id');
             $table->string('supplier_kode', 10)->unique();
-            $table->string('supplier_nama', 10)->unique();
-            $table->string('supplier_kode', 10)->unique();
+            $table->string('supplier_nama', 100);
+            $table->string('supplier_alamat', 255);
             $table->timestamps();
         });
     }
