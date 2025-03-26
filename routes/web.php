c17a599193dcd3556dc34a086abbfc004ffcf3dc
Malik Adzano Aryasatya Dharmaputera <adzanomalik@gmail.com>
2025-03-26 19:38:48 +0700

    Jobsheet 6 praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,8 @@ Route::group(['prefix' => 'user'], function () { // Ditandai
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
 });
 
 Route::group(['prefix' => 'level'], function () {
