9d9db58b14d07cbb9536f34bcf1f1bd58738b21f
Malik Adzano Aryasatya Dharmaputera <adzanomalik@gmail.com>
2025-05-15 14:01:39 +0700

    Jobsheet 7 Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,44 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SupplierController;
-use App\Http\Controllers\BarangController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\SupplierController;
 
-Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter {id}, maka harus berupa angka
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::pattern('id', '[0-9]+');
 
 Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'postlogin']);
-Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('login', [AuthController::class, 'postLogin']);
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth');
 
-Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
-    // masukkan semua route yang perlu autentikasi di sini
+Route::middleware(['authorize:ADM'])->group(function(){
+    Route::get('/level', [LevelController::class, 'index']);
+    Route::get('/kategori', [KategoriController::class, 'index']);
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/user/tambah', [UserController::class, 'tambah']);
+    Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('user.tambah_simpan');
+    Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+    Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
+    Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 });
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('user.tambah_simpan');
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 Route::get('/', [WelcomeController::class, 'index']);
 Route::group(['prefix' => 'user'], function () { // Ditandai
